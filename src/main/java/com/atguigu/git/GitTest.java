94f0e269510a8b98a04cf9d3ff8601fa952265fc
xkl999 <90943970+xkl999@users.noreply.github.com>
2022-05-25 22:15:58 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master commit");
         System.out.println("hot-fix commit");
         System.out.println("push commit");
+        System.out.println("pull commit");
     }
 }
